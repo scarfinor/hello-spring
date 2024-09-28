c78085342cc46bdcc00d5de2f2e6944c5259e1b8
scarfinor <scarfinor@gmail.com>
2024-09-27 23:21:46 -0500

    git commit Spring message with params

---
+++ hello-spring/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -0,0 +1,30 @@
+package org.launchcode.hello_spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    //Handeles request at path /hello
+   @GetMapping("hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello, Spring!";
+    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodBye() {
+        return "Goodbye, Spring!";
+    }
+
+    //Handles request of the form /hello?name=LaunchCode
+    @GetMapping("test")
+    @ResponseBody
+    public String helloWithQueryParam(String name) {
+        return  "hello, " + name + "!";
+    }
+}
