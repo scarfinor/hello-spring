8b58073d0445d6554e4f4083ca53220e3d83f381
scarfinor <scarfinor@gmail.com>
2024-09-28 11:18:51 -0500

    git commit Spring message with params and a form with submit button

--- hello-spring/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -4,51 +4,66 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping(value="testClass")
 public class HelloController {
 
-    //Handles request at path /hello
+    //Handles request at path /testClass/hello
    @GetMapping("hello")
-    @ResponseBody
+   //@ResponseBody
     public String hello() {
         return "Hello, Spring!";
     }
 
+    //lives at /testClass/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
+    //@ResponseBody
     public String goodBye() {
         return "Goodbye, Spring!";
     }
 
-    //Handles request of the form /test?name=LaunchCode
+    //Handles request of the form /testClass/test?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "test")
-    @ResponseBody
+    //@ResponseBody
     public String testWithQueryParam(String name) {
         return  "Hello, " + name + "!";
     }
 
-    //Handles request of the form /test/LaunchCode
+    //Handles request of the form /testClass/test/LaunchCode
     @GetMapping("test/{name}")
-    @ResponseBody
+    //@ResponseBody
     public String testWithPathParam(@PathVariable String name) {
        return "Hello " + name + "!";
     }
 
-    //Handles request of the /form
+    //Handles request of the /testClass/form
     @GetMapping("form")
-    @ResponseBody
+    //@ResponseBody
     public String form() {
-       return "<html>" +
-               "<body>" +
-               "<p>" +
-               "Please enter a name!" +
-               "</p>"  +
-               "<form action='test' method='post'>" + //submit a request to /test
-               "<input type='text' name='name'>" +
-               "<input type='submit' value='Submit'" +
-               "</form>" +
-               "</body>" +
-               "</html>";
+        return "<html>" +
+                "<body>" +
+                "<p>" +
+                "Please enter a name!" +
+                "</p>" +
+                "<form action='test' method='post'>" + //submit a request to /test
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Submit'" +
+                "</form>" +
+                "</body>" +
+                "</html>";
     }
 
+        // responds to /testClass
+        @GetMapping("")
+        //@ResponseBody
+        public String testClass() {
+            return "Hello, Richard";
+        }
 
+        // responds to /testClass/testMethod
+        @GetMapping("testMethod")
+        //@ResponseBody
+        public String testMethod() {
+            return "Hello, Richard";
+        }
 }
