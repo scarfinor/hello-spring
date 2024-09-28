f1639c71bf638a9aff392da6263868655d7d556a
scarfinor <scarfinor@gmail.com>
2024-09-28 13:12:32 -0500

    git commit Spring message with path params and a form with submit button with 2 request params

--- hello-spring/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -3,8 +3,7 @@ package org.launchcode.hello_spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@ResponseBody
+@RestController
 @RequestMapping("hello")
 public class HelloController {
     //handle request at path http://localhost:8080/hello
@@ -22,28 +21,71 @@ public class HelloController {
     }
 
     //handle requests of the form http://localhost:8080/hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!";
+    //@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+   // public String bonjourWithQueryParam(@RequestParam String name){
+     //   return "Bonjour, " + name + "!";
+    //}
+
+    @PostMapping
+    public String helloPost(@RequestParam(required = false) String name, @RequestParam String language) {
+        if (name == null || name.trim().isEmpty()) {
+            name = "World";
+        }
+
+        return createMessage(name, language);
     }
 
-    //handle requests of the form http://localhost:8080/hello/LaunchCode
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        switch (l.toLowerCase()) {
+            case "english":
+                greeting = "Hello";
+                break;
+            case "french":
+                greeting = "Bonjour";
+                break;
+            case "italian":
+                greeting = "Buongiorno";
+                break;
+            case "spanish":
+                greeting = "Hola";
+                break;
+            case "german":
+                greeting = "Hallo";
+                break;
+            default:
+                greeting = "Hello"; // Default fallback
+                break;
+        }
+
+        return "<html><body><h1>" + greeting + ", " + n + "!</h1></body></html>";
+    }
+
     @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
-    }
+    public String helloWithPathParam(@PathVariable String name) {
+        return "<html><body><h1>Hello, " + name + "!</h1></body></html>";
+   }
 
-    //handles requests for http://localhost:8080/hello/form
     @GetMapping("form")
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
-                "<input type = 'text' name = 'name' >" +
-                "<input type = 'submit' value = 'Greet Me!' >" +
+                "<h2>Greet Someone!</h2>" +
+                "<form action='/hello' method='post'>" +
+                "<input type='text' name='name' placeholder='Enter your name'>" +
+                "<label for='language-select'>Choose a language:</label>" +
+                "<select name='language' id='language-select'>" +
+                "<option value=''>--Please choose an option--</option>" +
+                "<option value='french'>French</option>" +
+                "<option value='english'>English</option>" +
+                "<option value='german'>German</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='italian'>Italian</option>" +
+                "</select>" +
+                "<input type='submit' value='Greet Me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
-
 }
