71409341ce5c79455d555099473bd6e13fadf00b
scarfinor <scarfinor@gmail.com>
2024-09-28 10:47:17 -0500

    git commit Spring message with params and a form with submit button

--- hello-spring/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -1,14 +1,12 @@
 package org.launchcode.hello_spring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
 
-    //Handeles request at path /hello
+    //Handles request at path /hello
    @GetMapping("hello")
     @ResponseBody
     public String hello() {
@@ -21,10 +19,36 @@ public class HelloController {
         return "Goodbye, Spring!";
     }
 
-    //Handles request of the form /hello?name=LaunchCode
-    @GetMapping("test")
+    //Handles request of the form /test?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "test")
     @ResponseBody
-    public String helloWithQueryParam(String name) {
-        return  "hello, " + name + "!";
+    public String testWithQueryParam(String name) {
+        return  "Hello, " + name + "!";
     }
+
+    //Handles request of the form /test/LaunchCode
+    @GetMapping("test/{name}")
+    @ResponseBody
+    public String testWithPathParam(@PathVariable String name) {
+       return "Hello " + name + "!";
+    }
+
+    //Handles request of the /form
+    @GetMapping("form")
+    @ResponseBody
+    public String form() {
+       return "<html>" +
+               "<body>" +
+               "<p>" +
+               "Please enter a name!" +
+               "</p>"  +
+               "<form action='test' method='post'>" + //submit a request to /test
+               "<input type='text' name='name'>" +
+               "<input type='submit' value='Submit'" +
+               "</form>" +
+               "</body>" +
+               "</html>";
+    }
+
+
 }
